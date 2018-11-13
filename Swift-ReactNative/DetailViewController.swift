eaf29fae98044a59649675c98c28219ef303e570
Tomas Sykora, jr <jump@syky.cz>
2018-11-13 23:36:51 +0100

    Initial Commit

---
+++ Swift-ReactNative/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Swift-ReactNative
+//
+//  Created by Tomas Sykora, jr. on 13/11/2018.
+//  Copyright © 2018 Tomas Sykora, jr. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
